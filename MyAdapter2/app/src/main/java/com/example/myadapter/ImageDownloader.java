6cee42e9a1426a6ebeeb787eba5285389ba372bf
manu-c7 <manu2942003@gmail.com>
2026-02-09 17:49:16 +0100

    Pro

---
+++ MyAdapter2/app/src/main/java/com/example/myadapter/ImageDownloader.java
@@ -0,0 +1,28 @@
+package com.example.myadapter;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.concurrent.Executors;
+
+public class ImageDownloader {
+
+    private final android.os.Handler handler = new Handler(Looper.getMainLooper());
+
+    public void download(String url, ImageView imageView) {
+        Executors.newSingleThreadExecutor().execute(() -> {
+            try {
+                InputStream inputStream = new URL(url).openStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                handler.post(() -> imageView.setImageBitmap(bitmap));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
